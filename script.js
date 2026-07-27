6737f317d5b4bb7242611ab64d1ddd2e5fbb7a5b
GOLDTASK2026 <miketuak@gmail.com>
2026-07-27 16:08:19 +0100

    Add ad watching and reward functions in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let balance = 0;
+
+function watchAd() {
+  alert("Ad started!");
+  balance += 10;
+  alert("You earned ₦10");
+}
+
+function dailyReward() {
+  alert("Daily reward received!");
+  balance += 50;
+}
+
+function withdraw() {
+  alert("Withdraw page coming soon");
+}
